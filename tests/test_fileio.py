e730fddf25a6fa825be65e7afb533d869e1383af
arantzazuLarrainzar <arantzazu.larrainzar@gmail.com>
2025-11-10 20:34:06 +0100

    Testing methods for FileIO class

---
+++ tests/test_fileio.py
@@ -0,0 +1,69 @@
+import os
+import unittest
+import tempfile
+import pandas as pd
+from modules import FileIO
+
+
+class TestFileIO(unittest.TestCase):
+    """
+    This class tests the methods implemented in FileIO class.
+    """
+    # methods
+    def test_read_existing_file(self):
+        """
+        This methods checks if the function `read_file` returns a byte object
+        from an existing file.
+        """
+        bytes_content = b"hello world!"
+        with tempfile.TemporaryFile(mode="w") as fp:
+            fp.write("hello world")
+            read_content = FileIO().read_file(fp.name)
+        assert bytes_content == read_content
+    
+    def test_read_non_existing_file(self):
+        """
+        This method checks if the function `read_file` returns a byte object
+        from file that do not exist. In this case, the method `read_file`
+        should return a None object.
+        """
+        # read the non-existing file
+        with self.assertLogs() as captured:
+            read_content = FileIO().read_file("./non_existing_file.csv")
+        # check if the method returns a None object and sends the proper logs
+        # messages
+        self.assertEqual(len(captured.records), 1)
+        self.assertEqual(
+            captured.records[0].getMessage(),
+            "The file in './non_existing_file.csv' does not exist.")
+        assert read_content is None
+    
+    def test_write_new_file(self):
+        """
+        This method checks if the method `write_csv` in FileIO saves a
+        pandas.DataFrame into a file that does not initially exist.
+        """
+        to_save_obj = pd.DataFrame({"feat1": [0, 1, 2], "feat2": [4, 5, 6]})
+        # save the DataFrame object
+        FileIO().write_csv("./data/write_test_method_new_file.csv", to_save_obj)
+        # read the saved information and remove the created file
+        saved_object = pd.read_csv("./data/write_test_method_new_file.csv")
+        os.remove("./data/write_test_method_new_file.csv")
+        # test the DataFrame object is equal to the saved object
+        pd.testing.assert_frame_equal(to_save_obj, saved_object)
+    
+    def test_write_old_file(self):
+        """
+        This method checks if the method `write_csv` in FileIO saves a
+        pandas.DataFrame into a file that exists previously.
+        """
+        to_save_obj = pd.DataFrame({"feat1": [0, 1, 2], "feat2": [4, 5, 6]})
+        # save the DataFrame object
+        FileIO().write_csv("./data/write_test_method_old_file.csv", to_save_obj)
+        # read the saved information and test if it is equal to the DataFrame object
+        saved_object = pd.read_csv("./data/write_test_method_old_file.csv")
+        pd.testing.assert_frame_equal(to_save_obj, saved_object)
+
+
+if __name__ == '__main__':
+    unittest.main()
